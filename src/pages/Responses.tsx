53d872298c06319329272cdc377664bbe7620128
arthur <contato.arthurreis222@gmail.com>
2025-08-31 09:32:23 -0300

    feat: add menus

---
+++ src/pages/Responses.tsx
@@ -0,0 +1,14 @@
+const ResponsesPage = () => {
+  return (
+    <div>
+      <header className="mb-12">
+        <h1 className="text-base font-bold tracking-tight">
+          Respostas Automáticas
+        </h1>
+      </header>
+      
+    </div>
+  );
+};
+
+export default ResponsesPage;
